aecafe3d50e6604497e0e3b7f2d20ab1786fb007
codewithwolf <saha86428@gmail.com>
2023-01-05 12:46:13 +0530

    first commit

---
+++ src/Sdata.js
@@ -0,0 +1,82 @@
+const Sdata=[
+    
+{
+    sname:"Dark",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABVLn6xY1bOI5mPeAZpU6v7wafhinJ0mnG3D1TKZM_Zv79mQIdxW9P41ZGyMcyxN4xI5vhRMyfF_6F4tsKCKxsGrXirLt-h32Ar1B.jpg?r=077",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80100172",
+},
+{
+    sname:"Stranger Things",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABer7SeWc6FvkBqWtk61GwL7rshAEVCOARQZVTEJGnLXykYBlO4nbbr6gs7M650BjULuaN6hucXKr5xY2iqPAajrxXd70HawdJeuD.jpg?r=608",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80057281",
+},
+
+{
+    sname:"Friends",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUcKPEIn68Fl3-Rj70s9Xy6o2xp-gmbQUsg7iyTlUmzYBZTrWk9QXVEzs4p74pLGlhqUHwcJKV7udY25cSYK7v98rfAX1clFU8A2.jpg?r=069",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/70153404",
+},
+
+{
+    sname:"Sacred games",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaV83uScxffcq2EMMTLXopYvQK78ay-C1VNi5dT-ph0bHZx_V-zPq0b-Q8tgLRYEjz2pNxIJTmQ2QbeP_yg4f7Q5nilHLYdBqg3c.jpg?r=6a2",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80115328",
+},
+
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+{
+    sname:"Money Hiest",
+    imgsrc:"https://occ-0-2610-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaJJIdyKMNvEcWhAtI5Xk12MQLY8DLxAkM2k8unVHyRordNL9Jn726cLoEmP_DAn44vwE3_l9dWTpDQTvmzsk6REyj4veXm9bvQ8.jpg?r=e19",
+    title: "A Netflix original",
+    links:"https://www.netflix.com/in/title/80192098",
+},
+
+
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import './App.css';
+import Sdata from './Sdata';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function mycard(val){
+  return(
+    <App
+    sname={val.sname}
+     imgsrc={val.imgsrc}
+    title={val.title}
+    link={val.links}
+ 
+ 
+  />
+  );
+
+}
+
+ReactDOM.render(
+ <>
+ <h1 className="heading_style">A <span>Netfilx</span> OrIgNioal <span>SeriEs</span></h1>
+     
+    
+      {Sdata.map(mycard)};
+   
+ </>, 
+  
+
+   document.getElementById("root")
+
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
